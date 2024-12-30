a50b5952da2c08634f5cf404fc8d28bdc42b27d7
ivy mondal <ivy94635@gmail.com>
2024-12-30 12:35:18 +0530

    they flying or crashing down?

--- index.js
+++ index.js
@@ -1,195 +1,77 @@
-const gameBoard = document.querySelector("#gameBoard");
-const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector("#scoreText");
-const resetBtn = document.querySelector("#resetBtn");
-const gameWidth = gameBoard.width;
-const gameHeight = gameBoard.height;
-const boardBackground = "forestgreen";
-const paddle1Color = "lightblue";
-const paddle2Color = "red";
-const paddleBorder = "black";
-const ballColor = "yellow";
-const ballBorderColor = "black";
-const ballRadius = 12.5;
-//const paddleSpeed = 50;
-let intervalID;
-let ballSpeed = 1;
-let ballX = gameWidth / 2;
-let ballY = gameHeight / 2;
-let ballXDirection = 0;
-let ballYDirection = 0;
-let player1Score = 0;
-let player2Score = 0;
-let paddle1 = {
-    width: 25,
-    height: 100,
-    x: 0,
-    y: 0
-}
-let paddle2 = {
-    width: 25,
-    height: 100,
-    x: gameWidth - 25,
-    y: gameHeight - 25
-}
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 600;
+document.body.appendChild(canvas);
 
-gameBoard.addEventListener("mousedown", updatePaddlePosition)
-resetBtn.addEventListener("click", resetGame);
+const BLACK = "#000000";
+const WHITE = "#FFFFFF";
+const PINK = "#D966BA";
+const ORANGE = "#E6733E";
+const BLUE = "#3EABE6";
+const GREEN = "#3FD18A";
 
-gameStart();
+class Ball {
+    constructor(x, y, radius, sx, sy, charge, mass) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.sx = sx;
+        this.sy = sy;
+        this.charge = charge;
+        this.mass = mass;
 
-
-function gameStart() {
-    createBall();
-    nextTick();
-}
-
-function nextTick() {
-    intervalID = setTimeout(() => {
-        clearBoard();
-        drawPaddles();
-        moveBall();
-        drawBall(ballX, ballY);
-        checkCollision();
-        nextTick();
-    }, 10)
-}
-
-function clearBoard() {
-    ctx.fillStyle = boardBackground;
-    ctx.fillRect(0, 0, gameWidth, gameHeight);
-}
-
-function drawPaddles() {
-    ctx.strokeStyle = paddleBorder;
-
-    ctx.fillStyle = paddle1Color;
-    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
-    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
-
-    ctx.fillStyle = paddle2Color;
-    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
-    ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
-}
-
-function createBall() {
-    ballSpeed = 1;
-    if(Math.round(Math.random()) === 1){
-        ballXDirection = 1;
-    }
-    else{
-        ballXDirection = -1;
-    }
-    if(Math.round(Math.random()) === 1){
-        ballYDirection = 1;
-    }
-    else{
-        ballYDirection = -1;
-    }
-    ballX = gameWidth / 2;
-    ballY = gameHeight / 2;
-    drawBall(ballX, ballY);
-}
-
-function moveBall() {
-    ballX += (ballSpeed * ballXDirection);
-    ballY += (ballSpeed * ballYDirection);
-}
-
-function drawBall(ballX, ballY) {
-    ctx.fillStyle = ballColor;
-    ctx.strokeStyle = ballBorderColor;
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-}
-
-function checkCollision() {
-    if(ballY <= ballRadius){
-        ballYDirection *= -1
-    }
-    if(ballY >= gameHeight - ballRadius){
-        ballYDirection *= -1;
-    }
-    if(ballX <= 0){
-        player2Score+=1;
-        updateScore();
-        createBall();
-        return;
-    }
-    if(ballX >= gameWidth){
-        player1Score+=1;
-        updateScore();
-        createBall();
-        return;
-    }
-    if(ballX <= (paddle1.x + paddle1.width + ballRadius)){
-        if(ballY > paddle1.y && ballY < paddle1.y + paddle1.height){
-            ballX = (paddle1.x + paddle1.width) + ballRadius; //if ball gets stuck
-            ballXDirection *= -1;
-            ballSpeed += 1;
+        if (charge > 0) {
+            this.color1 = Math.random() < 0.5 ? ORANGE : PINK;
+            this.color2 = WHITE;
+        } else if (charge < 0) {
+            this.color1 = Math.random() < 0.5 ? BLUE : GREEN;
+            this.color2 = WHITE;
+        } else {
+            this.color1 = BLACK;
+            this.color2 = WHITE;
         }
     }
-    if(ballX >= (paddle2.x - ballRadius)){
-        if(ballY > paddle2.y && ballY < paddle2.y + paddle2.height){
-            ballX = paddle2.x - ballRadius; //if ball gets stuck
-            ballXDirection *= -1;
-            ballSpeed += 1;
-        }
+    move() {
+        this.x += this.sx;
+        this.y += this.sy;
+    }
+    draw() {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+
+        ctx.clip();
+
+        ctx.fillStyle = this.color1;
+        ctx.fillRect(this.x - this.radius, this.y - this.radius, this.radius, this.radius * 2);
+
+        ctx.fillStyle = this.color2;
+        ctx.fillRect(this.x, this.y - this.radius, this.radius, this.radius * 2);
+
+        ctx.restore();
     }
 }
 
-let isDragging = false;
+const balls = [
+    new Ball(100, 50, 20, 3, 3, 1, 1),
+    new Ball(100, 100, 40, 1, 1, 2, 3),
+    new Ball(200, 200, 20, 3, 3, -1, 1),
+    new Ball(200, 400, 40, 1, 1, -2, 3),
+    new Ball(300, 200, 30, 2, 2, 0, 2)
+];
 
-//function handleMouseDown(event) {
- //   isDragging = true;
-//}
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas first
+    ctx.fillStyle = "#1A287A"; // Set the background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill the canvas with the color
 
-//function handleMouseUp(event) {
-    //isDragging = false;
-//}
+    balls.forEach(ball => {
+        ball.move();
+        ball.draw();
+    });
 
-function updatePaddlePosition(event) {
-    //if (!isDragging) return; // If we're not dragging, do nothing!
-
-    // Get mouse Y position relative to the canvas
-    const mouseY = event.clientY - gameBoard.getBoundingClientRect().top;
-
-    // Move paddles only when we're clicking and dragging
-    const newPosition = mouseY - paddle1.height/2;
-
-    if (newPosition >= 0 && newPosition <= gameHeight - paddle1.height) {
-        paddle1.y = newPosition;
-        paddle2.y = newPosition;
-    }
+    requestAnimationFrame(animate);
 }
 
-function updateScore(){
-    scoreText.textContent = `${player1Score} : ${player2Score}`;
-}
-function resetGame() {
-    player1Score = 0;
-    player2Score = 0;
-    paddle1 = {
-        width: 25,
-        height: 100,
-        x: 0,
-        y: 0
-    }
-    paddle2 = {
-        width: 25,
-        height: 100,
-        x: gameWidth - 25,
-        y: gameHeight - 100
-    }
-    ballSpeed = 1;
-    ballX = 0;
-    ballY = 0;
-    ballXDirection = 0;
-    ballYDirection = 0;
-    updateScore();
-    clearInterval(intervalID);
-    gameStart();
-}
+animate();
