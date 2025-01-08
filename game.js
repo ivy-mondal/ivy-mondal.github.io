5446bb8a1a271d5751f80655f81289adc24701bc
ivy mondal <ivy94635@gmail.com>
2025-01-08 18:32:07 +0530

    res 🐾 n logic added, yayy

---
+++ game.js
@@ -0,0 +1,38 @@
+export class Game{
+    constructor(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.balls = balls;
+    }
+
+    update(){
+        this.moveBalls();
+        this.respawnBalls();
+        this.checkCollisions();
+        this.drawBalls();
+    }
+
+    moveBalls(){
+        this.balls.forEach(ball => ball.move());
+    }
+
+    respawnBalls(){
+        this.balls.forEach(ball => {
+            if (
+                ball.x - ball.radius > this.canvas.width ||
+                ball.x + ball.radius < 0 ||
+                ball.y - ball.radius > this.canvas.height ||
+                ball.y + ball.radius < 0
+            ){
+                this.respawnBall(ball)
+            }
+        });
+    }
+
+    respawnBall(ball){
+        ball.x = Math.random() * this.canvas.width;
+        ball.y = Math.random() * this.canvas.height;
+        ball.sx = Math.random() * 5;
+        ball.sy = Math.random() * 5;
+    }
+}
